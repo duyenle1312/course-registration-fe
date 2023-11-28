23d822ddcf813b4aca20e0d215173e10e3ae3db6
duyenle1312 <duyenle131202@gmail.com>
2023-11-28 20:23:35 +0200

    add backend check status

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,22 @@
+"use client";
 import { CourseList } from "@/components/course-list";
 import { MainNav } from "@/components/main-nav";
 import { UserNav } from "@/components/user-nav";
+import { useEffect, useState } from "react";
 
-export default async function Home() {
+export default function Home() {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const API_url = process.env.NEXT_PUBLIC_BACKEND_URL;
+    fetch(`${API_url}/departments`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading === true) return <div className="flex justify-center my-80">Connecting to REST APIs...</div>;
   return (
     <>
       <MainNav />
